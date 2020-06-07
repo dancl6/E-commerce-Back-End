87ad8415804e8d0512c52c81f2fa5065bd7ab3b4
Dan Lyons <danielclyons@yahoo.com>
2020-06-06 17:14:08 -0700

    updated index.js

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'user_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'user_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_posts',
+  foreignKey: 'product_id'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_posts',
+  foreignKey: 'tag_id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
